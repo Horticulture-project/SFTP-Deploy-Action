69951ede1dd511cb182fb59d0f0831e1cc409c9b
Asbjørn Stokka <asstokka@online.no>
2021-07-22 02:40:44 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,9 +12,9 @@ printf "%s" "$4" >$TEMP_SSH_PRIVATE_KEY_FILE
 # avoid Permissions too open
 chmod 600 $TEMP_SSH_PRIVATE_KEY_FILE
 
-echo 'ssh start'
+#echo 'ssh start'
 
-ssh -o StrictHostKeyChecking=no -p $3 -i $TEMP_SSH_PRIVATE_KEY_FILE $1@$2 mkdir -p $6
+# ssh -o StrictHostKeyChecking=no -p $3 -i $TEMP_SSH_PRIVATE_KEY_FILE $1@$2 mkdir -p $6
 
 echo 'sftp start'
 # create a temporary file containing sftp commands
